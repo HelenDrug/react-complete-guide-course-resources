131d09583af3ece12661b35851f367759f1bf12d
HelenDrug <helendrugalya@gmail.com>
2025-07-07 11:25:16 +0200

    added Prettier and other changes

--- code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.tsx
+++ code/04 Essentials Deep Dive/07-tic-tac-toe-starting-project/src/App.tsx
@@ -1,17 +1,23 @@
-import type {ReactElement} from 'react';
+import type { ReactElement } from 'react';
+import Player from './components/Player';
 
 function App(): ReactElement {
-    return (
-        <main>
-            <div id={"game-container"}>
-                <ol id={"players"}>
-                    <li><span className={"player-name"}>Player 1</span></li>
-                    <li><span className={"player-symbol"}>X</span></li>
-                </ol>
-            </div>
-        </main>
-    );
+	return (
+		<>
+			<header>
+				<img src="/game-logo.png" alt="Game Logo" />
+				<h1>Tic-Tac-Toe</h1>
+			</header>
+			<main>
+				<div id={'game-container'}>
+					<ol id={'players'}>
+						<Player name={'Player 1'} symbol={'X'} />
+						<Player name={'Player 2'} symbol={'0'} />
+					</ol>
+				</div>
+			</main>
+		</>
+	);
 }
 
 export default App;
-
