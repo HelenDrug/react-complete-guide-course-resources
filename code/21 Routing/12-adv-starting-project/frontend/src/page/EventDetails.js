3220f2d18cc5fe2752659b528c4b4fb74c9b6ddc
HelenDrug <helendrugalya@gmail.com>
2025-08-14 16:07:29 +0200

    6 and 7. Output a list of dummy events to the EventsPage. Output the ID of the selected event on the EventDetailPage

--- code/21 Routing/12-adv-starting-project/frontend/src/page/EventDetails.js
+++ code/21 Routing/12-adv-starting-project/frontend/src/page/EventDetails.js
@@ -1,9 +1,12 @@
+import {useParams} from "react-router-dom";
+
 export default function EventDetails() {
+    const params = useParams()
     return (
         <>
             <h1>Event Details</h1>
             <p>Details for the selected event will be displayed here.</p>
-            <p>Event ID: {/* You can replace this with dynamic content */}123</p>
+            <p>Event ID: {params.eventId}</p>
         </>
     );
 }
--- code/21 Routing/12-adv-starting-project/frontend/src/page/Events.js
+++ code/21 Routing/12-adv-starting-project/frontend/src/page/Events.js
@@ -1,14 +1,25 @@
+import {Link} from "react-router-dom";
+
+const EVENTS = [
+    {id: 'event1', name: 'Event 1'},
+    {id: 'event2', name: 'Event 2'},
+    {id: 'event3', name: 'Event 3'},
+    {id: 'event4', name: 'Event 4'},
+    {id: 'event5', name: 'Event 5'},
+];
 export default function Events() {
     return (
         <>
         <h1>Events</h1>
         <p>Here is a list of events:</p>
         <ul>
-            <li><a href="/events/1">Event 1</a></li>
-            <li><a href="/events/2">Event 2</a></li>
-            <li><a href="/events/3">Event 3</a></li>
-            <li><a href="/events/4">Event 4</a></li>
-            <li><a href="/events/5">Event 5</a></li>
+            {
+                EVENTS.map(event => (
+                    <li key={event.id}>
+                        <Link to={event.id}>{event.name}</Link>
+                    </li>
+                ))
+            }
         </ul>
         </>
     );
