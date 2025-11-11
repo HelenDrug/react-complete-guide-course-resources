568bfb7a7bbf2d12a1bb7f7113936093770d0a01
HelenDrug <helendrugalya@gmail.com>
2025-11-11 14:33:21 +0100

    initial setup

--- code/25b RSC, Suspense, Server Actions/01-starting-project/app/layout.tsx
+++ code/25b RSC, Suspense, Server Actions/01-starting-project/app/layout.tsx
@@ -1,15 +1,15 @@
-import './globals.css';
+import "./globals.css";
 import type { ReactNode } from "react";
 
 export const metadata = {
-    title: 'React RSC, Server Actions & More',
-    description: 'A thorough demo of advanced React features.',
+  title: "React RSC, Server Actions & More",
+  description: "A thorough demo of advanced React features.",
 };
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-    return (
-        <html lang="en">
-        <body>{children}</body>
-        </html>
-    );
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
 }
