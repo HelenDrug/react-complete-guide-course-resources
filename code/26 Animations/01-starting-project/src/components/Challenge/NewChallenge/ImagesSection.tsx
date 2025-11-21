3bef2879e30aed03c4efc60b898569f002fd1e0f
HelenDrug <helendrugalya@gmail.com>
2025-11-21 13:55:04 +0100

    animate images

--- code/26 Animations/01-starting-project/src/components/Challenge/NewChallenge/ImagesSection.tsx
+++ code/26 Animations/01-starting-project/src/components/Challenge/NewChallenge/ImagesSection.tsx
@@ -1,5 +1,6 @@
 import images from "../../../assets/images.ts";
 import type { Image } from "../../../shared/types.ts";
+import {motion, stagger} from "framer-motion";
 
 interface ImagesSectionProps {
   onSelectImage: (image: Image) => void;
@@ -10,16 +11,28 @@ export default function ImagesSection({
   selectedImage,
 }: ImagesSectionProps) {
   return (
-    <ul id="new-challenge-images">
+    <motion.ul id="new-challenge-images" variants={{
+        visible: {
+          transition: {
+            delayChildren: stagger(0.1),
+          },
+        },
+        hidden: {},
+    }}>
       {images.map((image: Image) => (
-        <li
+        <motion.li
+          variants={{
+            hidden: { opacity: 0, scale: 0.5 },
+            visible: { opacity: 1, scale: 1 },
+          }}
+          transition={{ type: "spring" }}
           key={image.alt}
           onClick={() => onSelectImage(image)}
           className={selectedImage === image ? "selected" : undefined}
         >
           <img src={image.src} alt={image.alt} />
-        </li>
+        </motion.li>
       ))}
-    </ul>
+    </motion.ul>
   );
 }
