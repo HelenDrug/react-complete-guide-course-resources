45acf91772ebf4826c8c856d83e74efe2e8389c8
HelenDrug <helendrugalya@gmail.com>
2025-11-27 14:00:28 +0100

    refactoring of components for re-usability - WIP

--- code/27 Patterns/01-starting-project/src/App.jsx
+++ code/27 Patterns/01-starting-project/src/App.jsx
@@ -5,22 +5,22 @@ function App() {
         <section>
             <h2>Why work with us?</h2>
             <Accordion classname={"accordion"}>
-                <Accordion.Item className={"accordion-item"}>
-                    <Accordion.Title id={"experience"} className={"accordion-item-title"}>
+                <Accordion.Item id={"experience"} className={"accordion-item"}>
+                    <Accordion.Title className={"accordion-item-title"}>
                         We got 20 years of experience
                     </Accordion.Title>
-                    <Accordion.Content id={"experience"} className={"accordion-item-content"}>
+                    <Accordion.Content className={"accordion-item-content"}>
                         <article>
                             <p>You cannot go wrong with us!</p>
                         </article>
                     </Accordion.Content>
 
                 </Accordion.Item>
-                <Accordion.Item className={"accordion-item"}>
-                    <Accordion.Title id={"local"} className={"accordion-item-title"}>
+                <Accordion.Item id={"local"} className={"accordion-item"}>
+                    <Accordion.Title className={"accordion-item-title"}>
                         We are working with local guides
                     </Accordion.Title>
-                    <Accordion.Content id={"local"} className={"accordion-item-content"}>
+                    <Accordion.Content className={"accordion-item-content"}>
                         <article>
                             <p>We are mot doing it alone from our office</p>
                         </article>
--- code/27 Patterns/01-starting-project/src/components/Accordion/AccordionItem.jsx
+++ code/27 Patterns/01-starting-project/src/components/Accordion/AccordionItem.jsx
@@ -1,8 +1,21 @@
-export default function AccordionItem({className, children}) {
+import {createContext, useContext} from "react";
 
+const AccordionItemContext = createContext()
+
+export const useAccordionItemContext = () => {
+    const context = useContext(AccordionItemContext)
+    if(!context){
+        throw new Error("useAccordionItemContext must be used within an AccordionItemProvider")
+    }
+    return context
+}
+export default function AccordionItem({id, className, children}) {
     return (
-        <li className={className}>
-            {children}
-        </li>
+        <AccordionItemContext.Provider value={id}>
+            <li className={className}>
+                {children}
+            </li>
+        </AccordionItemContext.Provider>
+
     )
 }
